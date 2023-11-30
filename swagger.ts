94d244c54f9bd57f30a5bd78af9516bedc87a369
Aleksandr <onepopov@yandex.ru>
2023-11-30 04:09:30 +0400

    Fix: host temp

--- swagger.ts
+++ swagger.ts
@@ -1,10 +1,6 @@
 import swaggerAutogen from "swagger-autogen";
-import process from "node:process";
 // Todo
-const host =
-  process.env.NODE_ENV === "production"
-    ? "https://uppity-island-production.up.railway.app/"
-    : "localhost:8000";
+const host = "https://uppity-island-production.up.railway.app/";
 const doc = {
   info: {
     title: "AERO API",
